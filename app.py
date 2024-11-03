a75ce36088824c65ee2bdb133ee7991e2915f28e
Tomas Meskutavicius <32461666+TomasMeskutavicius@users.noreply.github.com>
2024-11-03 14:58:31 +0000

    some changes

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ JOBS = [{
     'id': 1,
     'title': 'Data Analyst',
     'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
+    'salary': '$10,00,000'
 }, {
     'id': 2,
     'title': 'Data Scientist',
     'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
+    'salary': '$15,00,000'
 }, {
     'id': 3,
     'title': 'Frontend Engineer',
